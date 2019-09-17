61dec6a413c6597cf7abb4a08d6055a03dbe0ea1
Wietse Wind <mail@wietse.com>
2019-09-17 16:07:55 +0200

    Get authed (PUSH) apps & revoke authed app

---
+++ src/api/v1/app/apps-push.js
@@ -0,0 +1,91 @@
+const log = require('debug')('app:curatedious')
+
+module.exports = async (req, res) => {
+  try {
+    const authorizations = await req.db(`
+      SELECT 
+        tokens.call_uuidv4 as authorization_uuidv4,
+        tokens.token_issued as authorization_issued,
+        tokens.token_expiration as authorization_expiration,
+        tokens.token_days_valid as authorization_days_valid,
+        applications.application_name,
+        applications.application_description,
+        applications.application_icon_url
+      FROM
+        tokens
+      LEFT JOIN
+        applications ON (
+          applications.application_id = tokens.application_id
+        )
+      WHERE
+        tokens.user_id = :userId
+      AND
+        tokens.token_hidden < 1
+      AND
+        tokens.token_reported < 1
+      AND
+        applications.application_disabled < 1
+    `, {
+      userId: req.__auth.user.id
+    })
+
+    switch (req.method) {
+      case 'GET': 
+        res.json({
+          authorizations: authorizations.map(d => {
+            return Object.keys(d).reduce((a, b) => {
+              const s = b.split('_')
+              if (typeof a[s[0]] === 'undefined') {
+                Object.assign(a, { [s[0]]: {} })
+              }
+              a[s[0]][s.slice(1).join('_')] = d[b]
+              return a
+            }, {})
+          })
+        })
+      break;
+
+      case 'DELETE':
+        if (typeof req.body.uuidv4 === 'undefined') {
+          const e = new Error('Authorizations UUIDv4 missing')
+          e.code = 404
+          throw e
+        } else if (authorizations.filter(a => {
+          return a.authorization_uuidv4 === req.body.uuidv4
+        }).length !== 1) {
+          const e = new Error('UUIDv4 invalid / previously revoked')
+          e.code = 500
+          throw e
+        } else {
+          res.json({revoked: true})
+          req.db(`
+            UPDATE 
+              tokens
+            SET
+              tokens.token_expiration = FROM_UNIXTIME(:expire),
+              tokens.token_hidden = 1,
+              tokens.token_reported = :report,
+              tokens.token_reported_desc = :reason
+            WHERE
+              tokens.user_id = :userId
+            AND
+              tokens.token_hidden < 1
+            AND
+              tokens.token_reported < 1
+            AND
+              tokens.call_uuidv4 = :uuidv4
+            LIMIT 1
+          `, {
+            userId: req.__auth.user.id,
+            uuidv4: req.body.uuidv4,
+            expire: new Date() / 1000,
+            report: typeof req.body.report_abuse !== 'undefined' && (req.body.report_abuse === 1 || req.body.report_abuse === true) ? 1 : 0,
+            reason: String(req.body.reason || '')
+          })
+        }
+      break;
+    }
+  } catch (e) {
+    res.handleError(e)
+  }
+}
--- src/handler/api.js
+++ src/handler/api.js
@@ -104,7 +104,8 @@ module.exports = async function (expressApp) {
           { method: [ 'get', 'patch', 'delete' ], path: 'pending-devices' },
           { method: [ 'get', 'patch' ], path: 'payload/:payloads__payload_id([0-9a-fA-F]{8}-[0-9a-fA-F]{4}-4[0-9a-fA-F]{3}-[89ABab][0-9a-fA-F]{3}-[0-9a-fA-F]{12})', module: 'payload' },
           { method: 'get', path: 'curated-ious' },
-          { method: 'get', path: 'account-info/:address(r[a-zA-Z0-9]{3,})', module: 'account-info' }
+          { method: 'get', path: 'account-info/:address(r[a-zA-Z0-9]{3,})', module: 'account-info' },
+          { method: [ 'get', 'delete' ], path: 'apps/push', module: 'apps-push' }
         ],
         errorHandler: errorHandler
       },
