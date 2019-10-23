c8459058c43f9e5bf24310f04ec49c9024e1c41d
Wietse Wind <mail@wietse.com>
2019-10-23 18:03:26 +0200

    Allow config multiple origins

--- src/middleware/cors.js
+++ src/middleware/cors.js
@@ -1,6 +1,10 @@
 module.exports = async function (expressApp) {
   expressApp.use((req, res, next) => {
-    let ValidOrigin = expressApp.config.AccessControlAllowOrigin || 'http://localhost:8080'
+    const ValidOrigins = expressApp.config.AccessControlAllowOrigins || []
+    const ValidOrigin = ValidOrigins.filter(o => {
+      return o === req.get('origin').split('/').slice(0, 3).join('/')
+    })[0] || 'http://localhost'
+    
     res.header('Access-Control-Allow-Origin', ValidOrigin) // TODO
     res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS')
     res.header('Access-Control-Allow-Credentials', 'true')
