affb3961a0225a2b9415b9d16412843d926a77b7
Wietse Wind <mail@wietse.com>
2019-10-24 13:50:29 +0200

    No origin (API) fallback

--- src/middleware/cors.js
+++ src/middleware/cors.js
@@ -2,7 +2,7 @@ module.exports = async function (expressApp) {
   expressApp.use((req, res, next) => {
     const ValidOrigins = expressApp.config.AccessControlAllowOrigins || []
     const ValidOrigin = ValidOrigins.filter(o => {
-      return o === req.get('origin').split('/').slice(0, 3).join('/')
+      return o === (req.get('origin') || '').split('/').slice(0, 3).join('/')
     })[0] || 'http://localhost'
     
     res.header('Access-Control-Allow-Origin', ValidOrigin) // TODO
