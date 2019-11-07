1a340c2ae3c32dff680ac5b5703da85891ddd13d
Wietse Wind <mail@wietse.com>
2019-11-07 17:25:40 +0100

    Fix multipart

--- src/middleware/cli-logger.js
+++ src/middleware/cli-logger.js
@@ -4,7 +4,7 @@ module.exports = async function (expressApp) {
   expressApp.use((req, res, next) => {
     if (typeof req.config.logRequestsToConsole !== 'undefined' && req.config.logRequestsToConsole) {
       if (!req.url.match(/\.(png|svg|css|js|ico)$/i)) {
-        log(`>> ${req.method} [${req.config.mode}, trusted: ${req.ipTrusted ? 1 : 0}] Got [${req.routeType}] call [${req.headers['content-type']||'NO CONTENT-TYPE'}] to [${req.url}] from ${req.remoteAddress}`)
+        log(`>> ${req.method} [${req.config.mode}, trusted: ${req.ipTrusted ? 1 : 0}] Got [${req.routeType}] call [${req.headers['content-type'] ? req.headers['content-type'].split(';')[0] : 'NO CONTENT-TYPE'}] to [${req.url}] from ${req.remoteAddress}`)
       }
     }
     next()
