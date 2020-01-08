8a0975aedc2e02c0863fc6482a7fce6281a7e6a1
Wietse Wind <mail@wietse.com>
2020-01-08 13:25:58 +0100

    Finish preliminary account advisory resolver

--- src/api/v1/app/account-advisory.js
+++ src/api/v1/app/account-advisory.js
@@ -1,6 +1,14 @@
+const advisory = require('@api/v1/internal/advisory')
+
+/**
+ * loadtest -n 2000 -c 100 \
+ *  -H 'Content-Type: application/json;' \
+ *  -H 'Authorization: Bearer 6a4d5d3b...' \
+ *  http://localhost:3001/api/v1/app/account-advisory/rPEPPER7kfTD9w2To4CQk6UCfuHM9c6GDY
+ */
+
 module.exports = async (req, res) => {
-  res.json({
-    dev: true,
-    params: req.params
-  })
+  const address = (req.params.address || '').trim()
+
+  res.json(await advisory(address.trim()))
 }
--- src/api/v1/internal/advisory.js
+++ src/api/v1/internal/advisory.js
@@ -1,8 +1,72 @@
 const fetch = require('node-fetch')
 const log = require('debug')('app:advisory')
 
-//   https://xrpforensics.org/api/advisory/advisory.json
+const ttlSeconds = 10
 
-module.exports = () => {
+const advisoryData = {
+  levels: {
+    E: 'ERROR',
+    0: 'UNKNOWN',
+    1: 'PROBABLE',
+    2: 'HIGH_PROBABILITY',
+    3: 'CONFIRMED'
+  },
+  accounts: {},
+  update: 0,
+  updating: false
+}
 
+const updateAdvisory = async () => {
+  advisoryData.updating = true
+
+  try {
+    const data = await fetch('https://xrpforensics.org/api/advisory/advisory.json')
+    const json = await data.json()
+
+    if (Object.keys(json).length < 100) {
+      throw new Error('Invalid advisory repsonse (keylen)')
+    }
+
+    advisoryData.update = Math.round(new Date() / 1000)
+    advisoryData.updating = false
+
+    Object.assign(advisoryData.accounts, json)
+
+    return true
+  } catch (e) {
+    advisoryData.updating = false
+    log('Error updating advisory data', e.message)
+
+    return false
+  }
+}
+
+module.exports = async (account) => {
+  if (!advisoryData.updating && advisoryData.update > 0 && advisoryData.update < Math.round(new Date() / 1000) - ttlSeconds) {
+    log('Update advisory data (TTL)')
+    updateAdvisory()
+  }
+  if (!advisoryData.updating && advisoryData.update === 0) {
+    log('<WAIT> Update advisory data (EMPTY)')
+    await updateAdvisory()
+  }
+
+  const danger = typeof advisoryData.accounts[account] === 'undefined'
+    ? advisoryData.levels[0 + '']
+    : advisoryData.levels[(advisoryData.accounts[account].status || 0) + ''] || advisoryData.levels['E']
+  
+  /**
+   * Todo: in case of multiple data soruces
+   */
+  const confirmations = {
+    xrpforensics: danger !== 'ERROR' && danger !== 'UNKNOWN' ? true : undefined
+  }
+
+  const advisoryForAccount = {
+    account,
+    danger,
+    confirmations
+  }
+
+  return advisoryForAccount
 }
