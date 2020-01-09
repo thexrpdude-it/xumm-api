0dfe410e6103236ecad67146d1eed37df7d4b48c
Wietse Wind <mail@wietse.com>
2020-01-09 14:13:46 +0100

    Add accout/slug resolver

--- src/api/v1/app/handle-lookup.js
+++ src/api/v1/app/handle-lookup.js
@@ -1,6 +1,14 @@
+const resolver = require('@api/v1/internal/handle-resolve')
+
+/**
+ * loadtest -n 2000 -c 100 \
+ *  -H 'Content-Type: application/json;' \
+ *  -H 'Authorization: Bearer 6a4d5d3b...' \
+ *  http://localhost:3001/api/v1/app/handle-lookup/wietse
+ */
+
 module.exports = async (req, res) => {
-  res.json({
-    dev: true,
-    params: req.params
-  })
+  const handle = (req.params.handle || '').trim()
+
+  res.json(await resolver(handle, req))
 }
--- src/api/v1/internal/handle-resolve.js
+++ src/api/v1/internal/handle-resolve.js
@@ -1,6 +1,299 @@
 const fetch = require('node-fetch')
 const log = require('debug')('app:handle-resolve')
 
-module.exports = async () => {
+const cacheSeconds = 60 * 15 // 15 minutes
 
+/**
+ * Data sources:
+ *  Bithomp API
+ *  Ripple Data API
+ *  XRPLNS
+ *  Internal database ^r....
+ */
+
+/**
+ * Todo: add XUMM hashed address book function lookup
+ */
+
+const is = {
+  validEmailAccount (query) {
+    const tester = /^[-!#$%&'*+\/0-9=?A-Z^_a-z`{|}~](\.?[-!#$%&'*+\/0-9=?A-Z^_a-z`{|}~])*@[a-zA-Z0-9](-*\.?[a-zA-Z0-9])*\.[a-zA-Z](-?[a-zA-Z0-9])+$/;
+
+    if (!query) {
+      return false
+    }
+
+    if (query.length > 254) {
+      return false
+    }
+    
+    const valid = tester.test(query)
+
+    if (!valid) {
+      return false
+    }
+
+    const parts = query.split('@')
+    if (parts[0].length > 64) {
+      return false
+    }
+    
+    const domainParts = parts[1].split('.')
+    if (domainParts.some(part => part.length > 63 )) {
+      return false
+    }
+
+    return true
+  },
+  possibleXrplAccount (query) {
+    return new RegExp(/^r[0-9a-zA-Z]{3,}$/).test(query)
+  }
+}
+
+const xrplns = {
+  networks: [],
+  initialized: false,
+  async call (url) {
+    const callApi = await fetch('https://api.xrplns.com/v1/' + url, {
+      headers: {
+        'XRPLNS-KEY': app.config.xrplnsKey || ''
+      },
+      method: 'get',
+      timeout: 3000
+    })
+    const json = await callApi.json()
+    return json
+  },
+  async initialize () {
+    log('Initializing XRPLNS, fetching social networks')
+    this.networks = await this.call('social-networks')
+    log('Initialized XRPLNS social networks', this.networks)
+  },
+  async get (query) {
+    const source = 'xrplns'
+    try {
+      const results = await (async () => {
+        if (is.validEmailAccount(query)) {
+          const callResults = await this.call('resolve/social/email/' + query)
+          return [Object.assign(callResults || {}, { network: 'email' })]
+        } else {
+          return Promise.all(this.networks.map(async n => {
+            const callResults = await this.call('resolve/social/' + n + '/' + query)
+            return Object.assign(callResults || {}, { network: n })
+          }).concat(await (async () => {
+            const callResults = await this.call('resolve/user/' + query)
+            if (callResults !== null &&
+              typeof callResults === 'object' &&
+              typeof callResults.data === 'object' &&
+              allResults.data !== null &&
+              typeof callResults.data.xrplAccounts === 'object'
+            ) {
+              const data = callResults.data.xrplAccounts
+              return data.map(d => {
+                return {
+                  network: 'local',
+                  data: d
+                }
+              })
+            }
+          })()))
+        }
+      })()
+
+      // log(results)
+
+      return results.filter(r => r !== null && typeof r === 'object' && typeof r.data === 'object' && typeof r.data.xrplAccount === 'string').map(r => {
+        return {
+          source,
+          network: r.network,
+          alias: r.data.slug || query,
+          account: r.data.xrplAccount,
+          tag: r.data.destinationTag === '' ? null : Number(r.data.destinationTag),
+          description: r.data.label,
+        }
+      })
+    } catch (e) {
+      log('Query @' + source + ' for [' + query + ']', e.message)
+    }
+    return []
+  }
+}
+
+const bithomp = {
+  async get (query) {
+    const source = 'bithomp.com'
+    try {
+      const call = await fetch('https://bithomp.com/api/v1/user/' + query, {
+        method: 'get',
+        timeout: 2000
+      })
+      const response = await call.json()
+      if (typeof response === 'object' && response !== null && typeof response.address === 'string') {
+        return [{
+          source,
+          network: null,
+          alias: response.username || query,
+          account: response.address,
+          tag: null,
+          description: ''
+        }]
+      }
+    } catch (e) {
+      log('Query @' + source + ' for [' + query + ']', e.message)
+    }
+    return []
+  }
+}
+
+const ripple = {
+  async get (query) {
+    const source = 'id.ripple.com'
+    try {
+      const call = await fetch('https://id.ripple.com/v1/user/' + query, {
+        method: 'get',
+        timeout: 2000
+      })
+      const response = await call.json()
+      if (typeof response === 'object' && response !== null && typeof response.address === 'string') {
+        return [{
+          source,
+          network: null,
+          alias: response.username || query,
+          account: response.address,
+          tag: null,
+          description: ''
+        }]
+      }
+    } catch (e) {
+      log('Query @' + source + ' for [' + query + ']', e.message)
+    }
+    return []
+  }
+}
+
+const internalAccounts = {
+  async get (query) {
+    const source = 'xumm.app'
+    if (is.possibleXrplAccount(query)) {
+      try {
+        const existing = await app.db(`
+          SELECT 
+            knownaccount_name,
+            knownaccount_account
+          FROM
+            knownaccounts
+          WHERE
+            knownaccount_account LIKE CONCAT(:knownaccount_account, '%')
+          AND
+            knownaccount_currency = ''
+          LIMIT 10
+        `, {
+          knownaccount_account: query
+        })
+
+        if (existing.length > 0) {
+          return existing.map(a => {
+            return {
+              source,
+              network: null,
+              alias: a.knownaccount_name,
+              account: a.knownaccount_account,
+              tag: null,
+              description: ''
+            }
+          })
+        }
+      } catch (e) {
+        log('Query @' + source + ' for [' + query + ']', e.message)
+      }
+    }
+    return []
+  }
+}
+
+const activeApps = [ bithomp, ripple, internalAccounts, xrplns ]
+
+const app = {
+  config: {},
+  db: null,
+  initializing: false,
+  initialized: false,
+  async initialize (config, db) {
+    this.config = config
+    this.db = db
+
+    if (!this.initializing) {
+      this.initializing = true
+      const reducedApps = activeApps.reduce((stack, current) => {
+        if (typeof current.initialized !== 'undefined' && typeof current.initialize !== 'undefined') {
+          stack.push(current.initialize())
+          current.initialized = true
+        }
+        return stack
+      }, [])
+      await Promise.all(reducedApps)
+      this.initialized = true
+      return this.initialized
+    }
+  }
+}
+
+const resolver = {
+  cache: {},
+  async get (query) {
+    const now = Math.round(new Date() / 1000)
+    if (typeof this.cache[query] === 'undefined' || this.cache[query].cached < now - cacheSeconds) {
+      this.cache[query] = {
+        cached: now,
+        explicitTests: {},
+        matches: {}
+      }
+
+      this.cache[query].explicitTests = {
+        emailAddress: is.validEmailAccount(query),
+        xrplAccount: is.possibleXrplAccount(query)
+      }
+    
+      const allApps = activeApps.reduce((stack, current) => {
+        stack.push(current.get(query))
+        return stack
+      }, [])
+  
+      this.cache[query].matches = await Promise.all(allApps).then(r => {
+        return r.reduce((stack, current) => {
+          current.forEach(c => stack.push(c))
+          return stack
+        }, [])
+      })
+    }
+
+    return Object.assign({
+      live: now === this.cache[query].cached
+    }, this.cache[query])
+  }
+}
+
+/**
+  * Samples:
+  *   rPdvC6ccq8hCdPKSPJkPmyZ4Mi1oG2FFkT
+  *   hi<at>wietse.com
+  *   WietseWind
+  *   pepperew
+  *   xrptipbot
+  *   tacostand
+  */
+
+module.exports = async (handle, req) => {
+  if (!app.initialized) {
+    log('Initializing')
+    await app.initialize(req.config, req.db)
+    log('Initialized')
+  }
+
+  const resolved = await resolver.get(handle.trim())
+
+  return {
+    input: handle,
+    ...resolved
+  }
 }
