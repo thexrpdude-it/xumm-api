eb1b772b16430584b452f7bc5f58f25642291d39
Wietse Wind <mail@wietse.com>
2020-01-31 16:35:01 +0100

    Add test environment

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  roots: ['<rootDir>/test'],
+  testEnvironment: 'node',
+  testRegex: '(.*|(\\.|/)(test|spec))\\.js?$',
+  moduleFileExtensions: ['js', 'jsx', 'json', 'node']
+}
