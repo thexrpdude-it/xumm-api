17aa11c58e03de01b8a53aa8ccbeb9ca03f8b716
Wietse Wind <mail@wietse.com>
2020-02-06 18:06:35 +0100

    Ignore bugsnag

--- src/handler/api.js
+++ src/handler/api.js
@@ -15,16 +15,8 @@ module.exports = async function (expressApp) {
   const errorHandler = async (e, req, res) => {
     // TODO: migrate to module
     if (typeof expressApp.config.bugsnagKey !== 'undefined') {
-      let err = e.causingError || e
-      if (typeof err === 'string') {
-        err = new Error(err)
-      }
-      expressApp.bugsnagClient.notify(Object.assign(err, {
-        code: undefined
-      }), {
-        metaData: Object.assign(req.__auth || {}, {
-          code: err.code || undefined
-        }),
+      expressApp.bugsnagClient.notify(e.causingError || e, {
+        metaData: req.__auth || {}
       })
     }
 
