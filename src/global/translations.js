9683a46952a9134badf416ae77be69c9c59edce8
Wietse Wind <mail@wietse.com>
2020-02-20 13:31:30 +0100

    Move translations

--- src/global/translations.js
+++ src/global/translations.js
@@ -1,12 +1,12 @@
 const fs = require('fs')
 const I18nFilter = require('@web/nunjucks_extensions/i18n')
 
-const files = fs.readdirSync(__dirname + '/../web/translations/')
+const files = fs.readdirSync(__dirname + '/../../translations/')
 let translations = []
 files.filter(f => {
   return f.match(/^([a-z]{2}|[a-z]{2}_[a-z]{2})\.js$/i)
 }).forEach(f => {
-  translations[f.slice(0, -3).toLowerCase()] = require('@src/web/translations/' + f.slice(0, -3))
+  translations[f.slice(0, -3).toLowerCase()] = require(__dirname + '/../../translations/' + f.slice(0, -3))
 })
 
 module.exports = {
