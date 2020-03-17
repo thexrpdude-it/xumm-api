df675f0d0ecb938a4afd060cabc19cde9bfb72a1
mp3yeur <mp3yeur@users.noreply.github.com>
2020-03-17 22:51:43 +0100

    Create _fr_fr.js
    French translation by XRPyeur
    
    Please verify :
    filename correct ?
    line 2 'fr'

---
+++ src/web/translations/_fr_fr.js
@@ -0,0 +1,60 @@
+﻿module.exports = {
+  LANGUAGE_CODE: 'fr',
+  ERROR: `Erreur`,
+  ERROR_404_BODY: `Désolé la page que vous demandez n'a pas pu être trouvée.`,
+  ERROR_500_BODY: `Désolé, votre demande ne peut pas être effectuée à cet instant.`,
+  LIPSUM: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin maximus sem ante, sit amet semper mauris venenatis non. Fusce posuere, libero ut volutpat ornare, massa ipsum hendrerit ipsum, sed finibus ipsum augue a nisl. Etiam at tempus ante, sit amet tristique leo. Vestibulum lobortis nisl a volutpat rhoncus. Ut facilisis ante a iaculis sollicitudin. Ut consectetur magna at libero vulputate, id pulvinar dui rutrum. Etiam facilisis finibus tincidunt.`,
+  BY_NAME: `XPRL Labs`,
+  XRPL_SIGNPLATFORM: `plateforme de signature XRPL (BETA)`,
+  TITLE_SIGNTX: `Demande de signature`,
+  ALREADY_SIGNED: `Déja signé`,
+  PAYLOAD_RESOLVED: `Cette demande à déjà été résolue (signé ou rejeté).`,
+  SCAN_WITH_APP: `Scannez ce QR-code avec <b class="font-logo">xumm</b>`,
+  PAYLOAD_NOT_FOUND: `Demande de signature introuvable.`,
+  PAYLOAD_EXPIRED: `Demande de signature expirée.`,
+  PAYLOAD_BACKEND_ERROR: `Désolé, une erreur s'est produite lors de la récupération des données à signer.`,
+  PAYLOAD_PAGE_FOR_MOBILE: `Pour signer une demande, veuillez vérifier que vous avez l'application <b class="font-logo">xumm</b> installée préalablement.`,
+  APPLICATION_DISABLED: `L'application <q><b>__appname__</b></q> est désactivée.`,
+  WAIT_CONNECT_PLATFORM: `Connection à la plateforme de signature.`,
+  WAIT_OPEN: `En attente que vous ouvriez la 
+                  <code class="text-primary"><b class="text-primary"><q>__txtype__</q></b></code>
+                  demande à l'aide l'application XUMM.`,
+  WAIT_SCAN_QR: `En attente de numérisation de 
+                  <code class="text-primary"><b class="text-primary"><q>__txtype__</q></b></code>
+                  la demande à l'aide de l'application XUMM.`,
+  WAIT_SCAN_QR_DETAILED: `En attente que vous numérisiez et ouvriez le 
+                            <code class="text-primary"><b class="text-primary">__txtype__</b></code>
+                          à 
+                            <code class="text-primary"><b>__txdestination__</b></code>
+                          avec l'application XUMM.`,
+  WAIT_FOR_RESOLVE: `En attente que vous acceptiez ou refusiez la 
+                      <code class="text-primary"><b class="text-primary"><q>__txtype__</q></b></code>
+                      demande dans l'application XUMM.`,
+  SHOW_TRANSACTION: `Afficher la demande de signature`,
+  RESOLVED: `La demande de signature à été signée correctement.`,
+  RESOLVED_NEXT_RETURN: `Vous allez retourner vers <q><b>__appname__</b></q> dans quelques secondes`.`,
+  REJECTED: `La demande de signature à été rejetée.`,
+  REJECTED_NEXT_RETURN: `Vous allez retourner vers <q><b>__appname__</b></q> dans quelques secondes`,
+  GET_APP_IOS_TITLE: `Téléchargez 𝘅𝗶𝗴𝗻 pour iOS sur l'application Apple App Store`,
+  GET_APP_ANDROID_TITLE: `Téléchargez 𝘅𝗶𝗴𝗻 pour Android sur l'application Google Play`,
+  MODAL_CLOSE: `Close`,
+  GET_APP_IOS_MODAL_TITLE: `Téléchargez <b class="font-logo">xumm</b> pour iOS`,
+  GET_APP_ANDROID_MODAL_TITLE: `Téléchargez <b class="font-logo">xumm</b> pour Android`,
+  GET_APP_IOS_MODAL_SCAN: `Numérisez le QR-code à l'aide de votre appareil iOS pour visiter le <i class="fab fa-apple"></i> la boutique Apple Store.`,
+  GET_APP_ANDROID_MODAL_SCAN: `Numérisez le QR-code à l'aide de votre appareil Android pour visiter le <i class="fab fa-google-play"></i> la boutique Google Play Store.`,
+  GET_APP_IOS_MODAL_VISIT: `Visitez la boutique d'applications Apple Store`,
+  GET_APP_ANDROID_MODAL_VISIT: `Visitez la boutique Google Play Store`,
+  PAYLOAD_PUSH_NOTIFICATION_SENT: `Une notification push à été envoyé à votre (vos) appareil(s) mobile(s).`,
+  DIDNT_RECEIVE_PUSH_TEXT: `Vous n'avez pas reçu la notification push ?`,
+  SHOW_QR: `Numériser le QR-code à la place`,
+  OPEN_IN_XUMM: 'Ouvrir dans l'application <b class="font-logo">xumm</b>',
+  PLEASE_INSTALL_APP: `Merci d'installer l'application <b class="font-logo">xumm</b>`,
+  DASH_OR: '-ou-',
+  INSTALL_APP: `Installer <b class="font-logo">xumm</b>`,
+  TITLE_TX_SCANNED: `Ouvert dans <b class="font-logo">xumm</b>`,
+  PAYLOAD_ALREADY_OPENED: `La demande de signature à déja été ouverte.`,
+  NEED_QR_PAYLOAD_QR: `Vous pouvez réouvrir la demande de signature. La demande de signature expire dans <span class="ts-moment">__in_x_time_expire__</span>.`,
+  PUSH_MSG_SIGN_REQUEST: `Demande de signature`,
+  PUSH_MSG_TXTYPE_TO_DEST: `__type__ à __destination__`,
+  PUSH_MSG_SIGNIN_REQ: `Connectez vous à __appname__`
+}
